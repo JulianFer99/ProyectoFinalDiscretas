f091991084c5e849ebbf6d7e5592fb76163dcc60
Julianfer <Julianfer@192.168.1.53>
2021-05-07 20:05:15 -0500

    ..

---
+++ ProyectoFinalDiscretas/src/run/Runner.java
@@ -0,0 +1,10 @@
+package run;
+
+import view.MainWindow;
+
+public class Runner {
+	public static void main(String[] args) {
+		MainWindow mainWindow = new MainWindow("Libertadores");
+		mainWindow.setVisible( true );
+	}
+}
--- ProyectoFinalDiscretas/src/view/MainWindow.java
+++ ProyectoFinalDiscretas/src/view/MainWindow.java
@@ -1,11 +1,109 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
 
 public class MainWindow extends JFrame {
- 
-	public MainWindow() {
-		// TODO Auto-generated constructor stub
-		; 
+	private JLabel title;
+	
+	private JMenuBar mb;
+    private JMenu menu;
+    private JMenuItem addBus, addDriver, information, calculateRoute, edit;
+    private JTextArea description;
+    
+//    private Maps
+
+	public MainWindow(String title) {
+		super(title);
+		setSize(new Dimension(480, 420));
+		setLayout(new GridBagLayout());
+		setResizable(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		beginComponents();
+		addComponents();
+	}
+
+	public void beginComponents() {
+		description = new JTextArea("Los Libertadores es un servicio de transporte \n"
+					+"de pasajeros entregado por Coflonorte, una empresa \n"
+					+"de transporte terrestre fundada el año 1953 en Boyacá \n"
+					+"con el nombre Flota Norte, tomando su nombre actual \n"
+					+"en el año 1965. Los Libertadores ofrecen sus servicios \n"
+					+"principalmente desde Bogotá con salida desde la \n"
+					+"Terminal Salitre. La empresa también ofrece, junto \n"
+					+"con el transporte de pasajeros, servicios de turismo, \n"
+					+"servicios personalizados y carga.");
+	
+	}
+
+	public void addComponents() {
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridy = 0;
+		gbc.gridx = 0;
+		gbc.gridwidth = 4;
+		title = new JLabel("Sistema de Recorridos Libertadores SAS");
+		title.setFont(new Font("Serif", Font.PLAIN, 25));
+		add(title, gbc);
+		gbc.gridwidth = 1;
+		
+		gbc.gridy = 1;
+		gbc.gridx = 0;
+		gbc.gridwidth = 4;
+		add(menu(), gbc);
+		gbc.gridwidth = 1;
+		
+		gbc.gridy = 2;
+		ImageIcon imgFlag = new ImageIcon("resources/bus.jpg");
+		JLabel bnrFlag = new JLabel();
+		bnrFlag.setIcon(imgFlag);
+		add(bnrFlag, gbc);
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridwidth = 1;
+		
+		gbc.gridy = 2;
+		gbc.gridx = 2;
+		add(new JLabel("      "), gbc);
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridwidth = 1;
+		
+		gbc.gridy = 2;
+		gbc.gridx = 3;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		description.setBackground(new Color(0, 0, 0, 0));
+		description.setFont(new Font("Serif", Font.PLAIN, 15));
+		add(description, gbc);
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridwidth = 1;
+	}
+
+	private Component menu() {
+		mb = new JMenuBar();
+		setJMenuBar(mb);
+		addBus = new JMenu("Registrar Bus");
+		mb.add(addBus);
+		addDriver = new JMenu("Registrar Conductor");
+		mb.add(addDriver);
+		information = new JMenu("Informacion");
+		mb.add(information);
+		calculateRoute = new JMenu("Calcular Recorrido");
+		mb.add(calculateRoute);
+		edit = new JMenu("Editar");
+		mb.add(edit);
+		
+		return mb;
 	}
 }
